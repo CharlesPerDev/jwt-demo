d633cae17ba0f06a09174da6157444ea591910c7
H-274 <65309260+H-274@users.noreply.github.com>
2024-04-05 21:22:04 +0000

    Fixed x-forward-host header issue

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@
  */
 await import("./src/env.js");
 
+let public_origin = process.env.PUBLIC_ORIGIN ?? "";
+
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [public_origin, "localhost:3000"],
+    },
+  },
+};
 
 export default config;
